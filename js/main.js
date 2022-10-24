b0f66580cadec5578d8f24213dda29e5096f79a1
eduardlima <eduardlima47@gmail.com>
2022-10-23 21:10:35 -0300

    atualizado

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 function logar(){
 
-      let login = document.getElementById('login').value;
+      let login = document.getElementById('email').value;
       let senha = document.getElementById('senha').value;
-
-      if(login == "admin" && senha == "admin"){
-        alert("sucesso");
+      
+      console.log(email.value+senha.value)
+      if(login == "admin@gmail.com" && senha == "admin"){
+        localStorage.setItem("acesso", true)
         location.href = "home.html";
       }else{
         alert('usuario ou senha incorreta')
       }
-
 }
